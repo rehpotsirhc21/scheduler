5ff37c470ed693835451653aa3ef1c7601563806
Christopher Langner <langnerc@icloud.com>
2022-04-10 16:17:12 -0500

    finished AC

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,14 @@
 const dateEl = document.querySelector("#currentDay");
 const textArea = document.getElementsByTagName("textarea");
 const btnEl = document.getElementsByClassName("btn");
-const idEl = document.getElementsByClassName("time");
+const parentDivEl = document.getElementsByClassName("time");
+
 
 
 //variables
 const date = moment().format("dddd, MMMM Do");
-let textAreaIndex;
-let timeBlockIndex;
-let savedArr = []
+
+
 
 //add date element to header
 dateEl.append(date);
@@ -18,62 +18,109 @@ dateEl.append(date);
 // loop for save event listener
 
 for (let i = 0; i < btnEl.length; i++) {
-  btnEl[i].addEventListener("click", saveData);
+  let btn = btnEl[i]
+  $(btn).on("click", saveData);
 }
 
 // function to highlight each timeblock based on relation to current time
-function getTimeBlock() {
+$(document).ready(function () {
   //local variables
   let currentTime = moment().format("H");
-    console.log(currentTime);
+    
   //iterate through the UI to color
   for (let i = 0; i < 9; i++) {
-    let getIdTime = parseInt(idEl[i].id);
+    let getIdTime = parseInt(parentDivEl[i].id);
 
     let getTextEl = textArea[i];
 
     if (getIdTime > currentTime) {
-      getTextEl.classList.add("bg-info", "text-white");
-    }
+      // getTextEl.classList.add("bg-info", "text-white");
+      $(getTextEl).addClass("bg-info text-white")
+      
+    } 
 
     if (getIdTime < currentTime) {
-      getTextEl.classList.add("bg-secondary", "text-white");
+     
+      $(getTextEl).addClass("bg-secondary text-white")
     }
 
     if (getIdTime == currentTime) {
-      getTextEl.classList.add("bg-success");
+      
+      $(getTextEl).addClass("bg-success")
+
     }
   }
-}
+} )
+
+
+$(document).ready(function  () {
+
+//  iterate through the UI to populate info from local storage
+  for (let i = 9; i < 18; i++) {
+
+    
+    let checkStorage = localStorage.getItem(i);
+    if (checkStorage == null) {
+      continue;
+    }
+   
+   
+    const parentDiv = document.getElementById(i);
+   
+   
+    const textAreaEl = parentDiv.getElementsByTagName("textarea")
+    $(textAreaEl).text(JSON.parse(checkStorage))
+    console.log(textAreaEl)
+     console.log(checkStorage)
+    
+    
+    
+     } 
+  
+   
+
+   
+    // console.log(checkStorage)
+  
+
+} )
 
 //function to save data
 // TODO: need to save data properly to local storage
 // TODO: need to grab data to show on refresh
 function saveData(event) {
   const btnClick = event.target;
-  const btnClickIndex = btnClick.id;
-  const btnClickValue = textArea[btnClickIndex].value;
-  
+  let getId = btnClick.parentNode.id
+  let textContent = btnClick.previousElementSibling.value
 
-  localStorage.setItem("index", btnClickIndex);
-  localStorage.setItem("text area", btnClickValue);
-  savedArr.push(btnClickValue);
+  console.log(btnClick.parentNode.id)
+  console.log(textContent)
 
-  for (let i = 0; i <savedArr.length; i++) {
-      console.log(savedArr[i]);
+  if (!textContent){
+    
+   
+    localStorage.removeItem(getId)
   }
+
+  localStorage.setItem(getId, JSON.stringify(textContent))
+  
+
+  
+  
+  // textArr.push(btnClickValue);
+  // indexArr.push(btnClickIndex);
+  // localStorage.setItem("text area", JSON.stringify(textArr));
+
+  // localStorage.setItem("index", JSON.stringify(indexArr));
+//  for (let i = 0; i <textArr.length; i++) {
+//   //     console.log(event.target)
+
+//   // } // 
   
   
 }
 
-function onLoad () {
-  const textAreaIndex = localStorage.getItem("index");
-  const textAreaContent = localStorage.getItem("text area")
-  console.log(textAreaIndex)
-  console.log(textAreaContent)
 
 
-}
 
-getTimeBlock();
-onLoad();
+
